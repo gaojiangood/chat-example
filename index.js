6804130538b63c2410c240c2efc7768eaac682c3
gaojiangood <gaojiangood@gmail.com>
2017-08-25 23:32:19 +0900

    add

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/index2', function(req, res){
+  res.sendFile(__dirname + '/index2.html');
+});
+
 io.on('connection', function(socket){
+  console.log('a user connected.');
   socket.on('chat message', function(msg){
+    console.log('message inputed :'+msg);
     io.emit('chat message', msg);
   });
 });
