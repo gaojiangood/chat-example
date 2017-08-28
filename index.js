68adc12996071c993a3a1c5788285212ceebe2fd
kendy <gaojiangood@gmail.com>
2017-08-28 18:23:13 +0900

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var usernum = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -13,11 +14,20 @@ app.get('/index2', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected.');
+  usernum++;
+  io.emit('chat message', 'a user connected.usernum:'+usernum);
   socket.on('chat message', function(msg){
     console.log('message inputed :'+msg);
     io.emit('chat message', msg);
   });
+  socket.on('disconnect', function(socket){
+    console.log('a user logouted.');
+    usernum--;
+    io.emit('chat message', 'a user logouted.usernum:'+usernum);
+  });
 });
+
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
